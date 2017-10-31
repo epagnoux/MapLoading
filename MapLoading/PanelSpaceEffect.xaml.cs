af47416b9424ae2dbb7401a60f5f4da83a480b4a
epagnoux <epagnoux@hotmail.com>
2017-10-30 23:40:04 -0400

    Fax alignement

--- MapLoading/PanelSpaceEffect.xaml.cs
+++ MapLoading/PanelSpaceEffect.xaml.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MapLoading
 {
-	/// <summary>
-	/// Interaction logic for PanelSpaceEffect.xaml
-	/// </summary>
-	public partial class PanelSpaceEffect : UserControl
-	{
-		public PanelSpaceEffect()
-		{
-			this.InitializeComponent();
-		}
-	}
+  /// <summary>
+  ///   Interaction logic for PanelSpaceEffect.xaml
+  /// </summary>
+  public partial class PanelSpaceEffect : UserControl
+  {
+    public static readonly DependencyProperty ContentProperty =
+      DependencyProperty.Register("Content",
+        typeof (object),
+        typeof (PanelSpaceEffect));
+
+
+    public PanelSpaceEffect()
+    {
+      InitializeComponent();
+    }
+
+    public object Content
+    {
+      get { return GetValue(ContentProperty); }
+      set { SetValue(ContentProperty, value); }
+    }
+  }
 }
