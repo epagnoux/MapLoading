f82d748c9bf3f67f4c26263eed0c634e786d6856
epagnoux <epagnoux@hotmail.com>
2017-11-12 22:55:21 -0500

    Add RotadeLoader

---
+++ MapLoading/Controls/RotadeLoader.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls.ColorEditor.ColorSchemas;
+
+namespace MapLoading
+{
+	/// <summary>
+	/// Interaction logic for RotadeLoader.xaml
+	/// </summary>
+	public partial class RotadeLoader : UserControl
+	{
+	  public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+	    "Color", typeof (Brush), typeof (RotadeLoader), new PropertyMetadata(Brushes.White));
+
+	  public Brush Color
+	  {
+	    get { return (Brush) GetValue(ColorProperty); }
+	    set { SetValue(ColorProperty, value); }
+	  }
+		public RotadeLoader()
+		{
+      if (double.IsNaN(Height))
+        Height = 100;
+      if (double.IsNaN(Width))
+		    Width = 100;
+		  
+			this.InitializeComponent();
+      SizeChanged += RotadeLoaderSizeChanged;
+		}
+
+    void RotadeLoaderSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      Height = Height = Math.Max(e.NewSize.Height, e.NewSize.Width);
+      Width = Height = Math.Max(e.NewSize.Height, e.NewSize.Width);
+    }
+	}
+}
---
+++ MapLoading/Converters/DoubleToDecreaseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MapLoading.Converters
+{
+  public class DoubleToDecreaseConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return value != null ? double.Parse(value.ToString()) - double.Parse(parameter.ToString()) : value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
